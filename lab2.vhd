c2b87a6f13c989e82aa3ce4623b939fdf48d96b5
Clement BOUILLOT <clementbouillot23@gmail.com>
2023-01-13 08:30:06 +0100

    compteur 3 digits avec bin_to_7seg composant

--- lab2.vhd
+++ lab2.vhd
@@ -1,31 +1,54 @@
-library IEEE;
-use IEEE.std_logic_1164.all;
-use IEEE.numeric_std.all;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity lab2 is
-	port(
-			clk : in std_logic;
-			reset : in std_logic;
-			pio_0 : out std_logic_vector(7 downto 0)
-	);
-end lab2;
+    port(
+        clk 		: in std_logic := '0';
+        reset 	: in std_logic :='0';
+		  pio_0 		: out std_logic_vector(7 downto 0);
+		  pio_1 		: out std_logic_vector(7 downto 0);
+		  pio_2 		: out std_logic_vector(7 downto 0)
+		  );
+end entity;
 
-
-architecture arch of lab2 is
-
-	component lab2_qsys is
+architecture rtl of lab2 is
+    component lab2_qsys is
         port (
-            clk_clk 											: in std_logic := 'X';  -- clk
-				reset_reset_n 									: in std_logic := 'X'; --reset_n
-				pio_0_external_connection_export			: out std_logic_vector(7 downto 0)
+				clk_clk        						: in  std_logic                    := 'X'; -- clk
+            reset_reset_n  						: in  std_logic                    := 'X'; -- reset_n
+            pio_0_external_connection_export : out std_logic_vector(3 downto 0);         -- export
+				pio_1_external_connection_export : out std_logic_vector(3 downto 0);         -- export
+				pio_2_external_connection_export : out std_logic_vector(3 downto 0)         -- export
         );
-   end component lab2_qsys;
-	begin
-
-    u0 : component lab2_qsys
-        port map (
-            clk_clk => clk,
-				reset_reset_n => reset,
-				pio_0_external_connection_export => pio_0
-        );
-end arch;
+    end component lab2_qsys;
+	 
+	 component bin_to_7seg is
+			port (
+					input: in std_logic_vector(3 downto 0) := (others => 'X');
+					output : out std_logic_vector(7 downto 0)
+			);
+	end component bin_to_7seg;
+	
+signal tempo_u : std_logic_vector(3 downto 0);
+signal tempo_d : std_logic_vector(3 downto 0);
+signal tempo_c : std_logic_vector(3 downto 0);
+begin
+	
+		u0 : component bin_to_7seg
+			port map (input => tempo_u,output => pio_0);
+			
+		u1 : component bin_to_7seg
+			port map (input => tempo_d,output => pio_1);
+			
+		u2 : component bin_to_7seg
+			port map (input => tempo_c,output => pio_2);
+				
+		u3 : component lab2_qsys
+			port map (clk_clk=>clk,
+						reset_reset_n=>reset,
+						pio_0_external_connection_export=>tempo_u,
+						pio_1_external_connection_export=>tempo_d,
+						pio_2_external_connection_export=>tempo_c
+						);  
+end architecture;
--- software/app/main.c
+++ software/app/main.c
@@ -3,14 +3,18 @@
 #include "altera_avalon_pio_regs.h"
 #include "sys/alt_irq.h"
 
-//tableau de génération des chiffres
-int cpt[10] =  {0b00111111, 0b00000110, 0b01011011, 0b01001111, 0b01100110, 0b01101101, 0b01111101, 0b00000111, 0b01111111, 0b01101111};
-
+//VERSION Q3
 int main(){
 	while(1){
-		for (int i=0; i<10;i++){
-			IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,~cpt[i]);
-			usleep(125000);
+		for (unsigned int k=0; k<10;k++){
+			IOWR_ALTERA_AVALON_PIO_DATA(PIO_2_BASE,k);
+			for (unsigned int j=0; j<10;j++){
+				IOWR_ALTERA_AVALON_PIO_DATA(PIO_1_BASE,j);
+				for (unsigned int i=0; i<10;i++){
+					IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,i);
+					usleep(60000);
+				}
+			}
 		}
 	}
 	return 0;
