4a1a03063f517b7f4dfee600d7371cbc51ec4de1
Clement BOUILLOT <clementbouillot23@gmail.com>
2023-01-13 08:35:01 +0100

    compteur 3 digits avec un seul pio (Q4)

--- lab2.vhd
+++ lab2.vhd
@@ -4,11 +4,9 @@ use ieee.numeric_std.all;
 
 entity lab2 is
     port(
-        clk 		: in std_logic := '0';
-        reset 	: in std_logic :='0';
-		  pio_0 		: out std_logic_vector(7 downto 0);
-		  pio_1 		: out std_logic_vector(7 downto 0);
-		  pio_2 		: out std_logic_vector(7 downto 0)
+        clk : in std_logic := '0';
+        reset : in std_logic :='0';
+		  pio_0 : out std_logic_vector(23 downto 0) := (others => '0')
 		  );
 end entity;
 
@@ -17,9 +15,7 @@ architecture rtl of lab2 is
         port (
 				clk_clk        						: in  std_logic                    := 'X'; -- clk
             reset_reset_n  						: in  std_logic                    := 'X'; -- reset_n
-            pio_0_external_connection_export : out std_logic_vector(3 downto 0);         -- export
-				pio_1_external_connection_export : out std_logic_vector(3 downto 0);         -- export
-				pio_2_external_connection_export : out std_logic_vector(3 downto 0)         -- export
+            pio_0_external_connection_export : out std_logic_vector(11 downto 0)         -- export
         );
     end component lab2_qsys;
 	 
@@ -30,25 +26,20 @@ architecture rtl of lab2 is
 			);
 	end component bin_to_7seg;
 	
-signal tempo_u : std_logic_vector(3 downto 0);
-signal tempo_d : std_logic_vector(3 downto 0);
-signal tempo_c : std_logic_vector(3 downto 0);
+signal tempo : std_logic_vector(11 downto 0);
 begin
 	
-		u0 : component bin_to_7seg
-			port map (input => tempo_u,output => pio_0);
-			
-		u1 : component bin_to_7seg
-			port map (input => tempo_d,output => pio_1);
-			
-		u2 : component bin_to_7seg
-			port map (input => tempo_c,output => pio_2);
+		unit : component bin_to_7seg
+			port map (input => tempo(3 downto 0),output => pio_0(7 downto 0));
+		
+		decimal : component bin_to_7seg
+			port map (input => tempo(7 downto 4),output => pio_0(15 downto 8));
+		
+		centaine : component bin_to_7seg
+			port map (input => tempo(11 downto 8),output => pio_0(23 downto 16));
 				
-		u3 : component lab2_qsys
+		u1 : component lab2_qsys
 			port map (clk_clk=>clk,
 						reset_reset_n=>reset,
-						pio_0_external_connection_export=>tempo_u,
-						pio_1_external_connection_export=>tempo_d,
-						pio_2_external_connection_export=>tempo_c
-						);  
+						pio_0_external_connection_export=>tempo);  
 end architecture;
--- software/app/main.c
+++ software/app/main.c
@@ -3,15 +3,14 @@
 #include "altera_avalon_pio_regs.h"
 #include "sys/alt_irq.h"
 
-//VERSION Q3
 int main(){
 	while(1){
 		for (unsigned int k=0; k<10;k++){
-			IOWR_ALTERA_AVALON_PIO_DATA(PIO_2_BASE,k);
+			IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,k<<8);
 			for (unsigned int j=0; j<10;j++){
-				IOWR_ALTERA_AVALON_PIO_DATA(PIO_1_BASE,j);
+				IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,j<<4);
 				for (unsigned int i=0; i<10;i++){
-					IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,i);
+					IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,(k<<8) | (j<<4) | i);
 					usleep(60000);
 				}
 			}
